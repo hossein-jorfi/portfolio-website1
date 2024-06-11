c42000e8daa97172d7eba3a0096c374751ca8021
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-06-11 11:07:44 +0330

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="main_paper">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+      },
+      overlay: {
+        initialIsOpen: false,
+      },
+    }),
+  ],
+});
