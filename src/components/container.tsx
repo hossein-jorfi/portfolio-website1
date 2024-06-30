0ebef1c7b869052ab83c4d08b5ada6b03e2c59fc
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-06-30 14:58:38 +0330

    edti & delete icons

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,7 +1,21 @@
 import { ReactNode } from "react";
+import { Element } from "react-scroll";
 
-const Container = ({ children }: { children: ReactNode }) => {
-  return <div className="flex justify-center h-full container p-10">{children}</div>;
+const Container = ({
+  children,
+  scrollTarget = "",
+}: {
+  children: ReactNode;
+  scrollTarget?: string;
+}) => {
+  return (
+    <Element
+      name={scrollTarget}
+      className="flex justify-center h-full container p-10"
+    >
+      {children}
+    </Element>
+  );
 };
 
 export default Container;
