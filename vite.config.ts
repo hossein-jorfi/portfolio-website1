51b0d916d982d7c2da4af91e7d4ed14196641ecf
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-06-30 13:23:17 +0330

    vite svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    svgr(),
     react(),
     checker({
       typescript: true,
