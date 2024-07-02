a491a1ab18d2992fb2e989d1748d8b22e00b2fb4
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-02 09:49:49 +0330

    className prop for button

--- src/components/button-type1.tsx
+++ src/components/button-type1.tsx
@@ -4,13 +4,18 @@ import { motion } from "framer-motion";
 type ButtonType1Props = {
   children: ReactNode;
   onClick?: () => void;
+  className?: string;
 };
 
-const ButtonType1 = ({ children, onClick = () => {} }: ButtonType1Props) => {
+const ButtonType1 = ({
+  children,
+  onClick = () => {},
+  className,
+}: ButtonType1Props) => {
   return (
     <motion.button
       onClick={onClick}
-      className="border-2 p-3 rounded-2xl text-primary text-lg"
+      className={`border-2 p-3 rounded-2xl text-primary text-lg ${className}`}
     >
       {children}
     </motion.button>
