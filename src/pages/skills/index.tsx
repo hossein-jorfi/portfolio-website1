ac36d3dc607e95f70e9faf71ce132948813f6c63
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-02 14:55:38 +0330

    skil

--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -1,6 +1,9 @@
 // constants
 import { SKILLS } from "./contants";
 
+// utils
+import { motion } from "framer-motion";
+
 // components
 import Container from "../../components/container";
 import Skill from "./skill";
@@ -15,7 +18,22 @@ const Skills = () => {
 
         <div className="grid grid-cols-4 gap-5">
           {SKILLS.map((skill) => (
-            <Skill {...skill} />
+            <Skill key={skill.id} {...skill}>
+              {"<h1>Hello World</h1>".split("").map((el, i) => (
+                <motion.span
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{
+                    duration: 0.25,
+                    delay: i / 10,
+                  }}
+                  key={i}
+                  className="text-3xl"
+                >
+                  {el}
+                </motion.span>
+              ))}
+            </Skill>
           ))}
         </div>
       </div>
--- src/pages/skills/skill.tsx
+++ src/pages/skills/skill.tsx
@@ -1,9 +1,17 @@
+import { ReactNode } from "react";
+
 type SkillProps = {
   title: string;
+  children: ReactNode;
 };
 
-const Skill = ({ title }: SkillProps) => {
-  return <div className="text-primary border-2 h-60 p-4 rounded-lg">{title}</div>;
+const Skill = ({ title, children }: SkillProps) => {
+  return (
+    <div className="text-primary border-2 h-60 p-4 rounded-lg">
+      <p>{title}</p>
+      {children}
+    </div>
+  );
 };
 
 export default Skill;
