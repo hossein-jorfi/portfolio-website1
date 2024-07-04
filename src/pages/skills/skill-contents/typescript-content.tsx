5124eb29347ccdfa18b629e96fddb45a314f4b00
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-04 15:24:13 +0330

    error underline component

--- src/pages/skills/skill-contents/typescript-content.tsx
+++ src/pages/skills/skill-contents/typescript-content.tsx
@@ -1,9 +1,13 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
+import { ReactNode } from "react";
 
 const TypeScriptContetnt = () => {
+  //   let name = 'hossein'
+  //   name = 10
+  //   console.log(name)
   return (
-    <div>
-      {"const name = 'Hossein'".split("").map((el, i) => (
+    <div className="text-2xl">
+      {/* {"const name = 'Hossein'".split("").map((el, i) => (
         <motion.span
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -16,9 +20,21 @@ const TypeScriptContetnt = () => {
         >
           {el}
         </motion.span>
-      ))}
+      ))} */}
+      <p>let name = "Hossein"</p>
+      <p>
+        <ErrorUnderLine>name</ErrorUnderLine> = 10
+      </p>
+      <p>ts: </p>
     </div>
   );
 };
 
 export default TypeScriptContetnt;
+
+const ErrorUnderLine = ({ children }: { children: ReactNode }) => (
+  <span className="relative w-fit">
+    <span>{children}</span>
+    <span className="w-full h-[2px] bg-red-700" />
+  </span>
+);
