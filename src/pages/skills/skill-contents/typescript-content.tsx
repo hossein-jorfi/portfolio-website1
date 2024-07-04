58f18eb318236c9ae47823dff5351df234f31f09
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-04 15:04:49 +0330

    ts contetnt component

--- src/pages/skills/skill-contents/typescript-content.tsx
+++ src/pages/skills/skill-contents/typescript-content.tsx
@@ -1,19 +1,24 @@
 import { motion } from "framer-motion";
 
-const TypeScriptContetnt = () =>
-  "<h1>Hello World</h1>".split("").map((el, i) => (
-    <motion.span
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{
-        duration: 0.25,
-        delay: i / 10,
-      }}
-      key={i}
-      className="text-3xl"
-    >
-      {el}
-    </motion.span>
-  ));
+const TypeScriptContetnt = () => {
+  return (
+    <div>
+      {"const name = 'Hossein'".split("").map((el, i) => (
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 0.25,
+            delay: i / 10,
+          }}
+          key={i}
+          className="text-2xl"
+        >
+          {el}
+        </motion.span>
+      ))}
+    </div>
+  );
+};
 
 export default TypeScriptContetnt;
