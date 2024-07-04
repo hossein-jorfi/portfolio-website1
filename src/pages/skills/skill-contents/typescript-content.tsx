ac3599a8226105f44e4b46a5fc2a473332d473eb
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-04 16:10:23 +0330

    tooltip

--- src/pages/skills/skill-contents/typescript-content.tsx
+++ src/pages/skills/skill-contents/typescript-content.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 // styles
 import styles from "./typescript-content.module.scss";
 
@@ -19,12 +19,27 @@ const ErrorUnderLine = ({ children }: { children: ReactNode }) => {
   //   let name = "hossein";
   //   name = 10;
   //   console.log(name);
+  const [showTooltip, setShowTooltip] = useState(false);
   return (
     <span className="relative w-fit">
       <span className="z-10">{children}</span>
-      <span className="w-full absolute left-0 -bottom-1">
+      <span
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+        className="w-full absolute left-0 -bottom-1"
+      >
         <span className={styles.zigzag} />
       </span>
+
+      {showTooltip && (
+        <span
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => setShowTooltip(false)}
+          className="w-32 border-2 absolute left-0 -bottom-12"
+        >
+          Hello World
+        </span>
+      )}
     </span>
   );
 };
