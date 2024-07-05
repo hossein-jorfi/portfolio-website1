53c75d9f8bee7834c88dcf4e0abb551ff0841123
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-05 15:44:03 +0330

    edit html content

--- src/pages/skills/skill-contents/html-content.tsx
+++ src/pages/skills/skill-contents/html-content.tsx
@@ -1,32 +1,49 @@
 import { motion } from "framer-motion";
 import { ElemVarName, ElemWhite } from "../../../components/editor-elemnts";
 
-const HTMLContetnt = () =>
-  "<h1>Hello World</h1>".split("").map((el, i) => (
-    <motion.span
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{
-        duration: 0.25,
-        delay: i / 10,
-      }}
-      key={i}
-      className="text-3xl"
-    >
-      {el === "<" ? (
-        <ElemWhite>{el}</ElemWhite>
-      ) : el === "h" ? (
-        <ElemVarName>{el}</ElemVarName>
-      ) : el === "1" ? (
-        <ElemVarName>{el}</ElemVarName>
-      ) : el === ">" ? (
-        <ElemWhite>{el}</ElemWhite>
-      ) : el === "/" ? (
-        <ElemWhite>{el}</ElemWhite>
-      ) : (
-        <ElemWhite>{el}</ElemWhite>
-      )}
-    </motion.span>
-  ));
+const HTMLContetnt = () => {
+  return (
+    <div className="text-3xl">
+      <div>
+        <ElemWhite>{"<"}</ElemWhite>
+        <ElemVarName>div</ElemVarName>
+        <ElemWhite>{">"}</ElemWhite>
+      </div>
+      <div className="ml-5"> 
+        {"<h1>Hello World</h1>".split("").map((el, i) => (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.25,
+              delay: i / 10,
+            }}
+            key={i}
+          >
+            {el === "<" ? (
+              <ElemWhite>{el}</ElemWhite>
+            ) : el === "h" ? (
+              <ElemVarName>{el}</ElemVarName>
+            ) : el === "1" ? (
+              <ElemVarName>{el}</ElemVarName>
+            ) : el === ">" ? (
+              <ElemWhite>{el}</ElemWhite>
+            ) : el === "/" ? (
+              <ElemWhite>{el}</ElemWhite>
+            ) : (
+              <ElemWhite>{el}</ElemWhite>
+            )}
+          </motion.span>
+        ))}
+      </div>
+      <div>
+        <ElemWhite>{"<"}</ElemWhite>
+        <ElemWhite>/</ElemWhite>
+        <ElemVarName>div</ElemVarName>
+        <ElemWhite>{">"}</ElemWhite>
+      </div>
+    </div>
+  );
+};
 
 export default HTMLContetnt;
