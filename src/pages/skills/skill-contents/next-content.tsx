5ef90c46d13a30d0b506976837b2cab6f6c55468
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-06 14:34:19 +0330

    assets

--- src/pages/skills/skill-contents/next-content.tsx
+++ src/pages/skills/skill-contents/next-content.tsx
@@ -1,5 +1,39 @@
+// assets
+import ServerShape from "../../../assets/common/server.svg?react";
+
+// utils
+// import Xarrow from "react-xarrows";
+
+// components
+
 const NextContent = () => {
-  return <div>NextContent</div>;
+  return (
+    <div>
+      <ServerBox />
+    </div>
+  );
 };
 
 export default NextContent;
+
+const ServerBox = () => (
+  <div>
+    {/* <div className="w-8 h-16 border-2" /> */}
+    <ServerShape width={50} height={50} />
+  </div>
+);
+
+// const Line = ({ start, end }: { start: string; end: string }) => {
+//   return (
+//     <Xarrow
+//       start={start}
+//       end={end}
+//       color={"rgb(125 211 252)"}
+//       showHead={false}
+//       strokeWidth={2}
+//       startAnchor="bottom"
+//       endAnchor="top"
+//       path="grid"
+//     />
+//   );
+// };
