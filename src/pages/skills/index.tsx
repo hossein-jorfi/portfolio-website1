b6a831506c89aa97dd78dda82e89b5b66ce46291
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-06 15:52:24 +0330

    styles

--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -15,7 +15,7 @@ const Skills = () => {
           <p className="title">Skills</p>
         </div> */}
 
-        <div className="grid grid-cols-4 gap-5 mt-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-10">
           {SKILLS.map((skill) => (
             <Skill key={skill.id} {...skill}>
               {skill.content}
