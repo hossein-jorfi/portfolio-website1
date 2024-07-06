4527ceaca96bdaa2b74f6275e4cc25898e1810df
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-06 12:46:18 +0330

    install Xarrow

--- src/pages/skills/skill-contents/react-content.tsx
+++ src/pages/skills/skill-contents/react-content.tsx
@@ -1,28 +1,32 @@
+import Xarrow, { Xwrapper } from "react-xarrows";
+
 const ReactContent = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex justify-center items-center">
-        <Box />
+    <Xwrapper>
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-center items-center">
+          <Box id="1" />
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <Box id="2" />
+          <Box id="3" />
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <Box id="4" />
+          <Box id="5" />
+          <Box id="6" />
+        </div>
       </div>
-      <div className="flex justify-center items-center gap-4">
-        <Box />
-        <Box />
-      </div>
-      <div className="flex justify-center items-center gap-4">
-        <Box />
-        <Box />
-        <Box />
-      </div>
-    </div>
+    </Xwrapper>
   );
 };
 
 export default ReactContent;
 
-const Box = () => {
+const Box = ({ id }: { id: string }) => {
   return (
     <>
-      <div className="w-16 h-8 border-2 border-sky-300 rounded-lg" />
+      <div id={id} className="w-16 h-8 border-2 border-sky-300 rounded-lg" />
     </>
   );
 };
