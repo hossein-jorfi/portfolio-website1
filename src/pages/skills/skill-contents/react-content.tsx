1046f7fd56774a993c5d0d5e3bf564269119a3e1
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-06 12:41:09 +0330

    styles

--- src/pages/skills/skill-contents/react-content.tsx
+++ src/pages/skills/skill-contents/react-content.tsx
@@ -1,5 +1,23 @@
 const ReactContent = () => {
-  return <div>ReactContent</div>;
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex justify-center items-center">
+        <Box />
+      </div>
+      <div className="flex justify-center items-center gap-3">
+        <Box />
+        <Box />
+      </div>
+    </div>
+  );
 };
 
 export default ReactContent;
+
+const Box = () => {
+  return (
+    <>
+      <div className="w-16 h-8 border-2 border-sky-300 rounded-lg" />
+    </>
+  );
+};
