bcf01e6d3953dac15cf02f6ce66f6e9986fcfcd5
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-16 09:42:59 +0330

    delete main bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { router } from "./routes";
 
 function App() {
   return (
-    <div className="main_bg">
-      <div className="main_paper">
-        <RouterProvider router={router} />
-      </div>
+    <div className="main_paper">
+      <RouterProvider router={router} />
     </div>
   );
 }
