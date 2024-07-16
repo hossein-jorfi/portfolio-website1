cd0b4fde602826cf542fabf48960358d2a8e2cde
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-16 11:12:19 +0330

    ReactContent

--- src/pages/skills/skill-contents/react-content.tsx
+++ src/pages/skills/skill-contents/react-content.tsx
@@ -5,21 +5,21 @@ const ReactContent = () => {
     <Xwrapper>
       <div className="flex flex-col gap-6">
         <div className="flex justify-center items-center">
-          <Box id="1" />
+          <Box id="1" title="App" />
           <Line start="1" end="2" />
           <Line start="1" end="3" />
         </div>
         <div className="flex justify-center items-center gap-4">
-          <Box id="2" />
-          <Box id="3" />
+          <Box id="2" title="Parent" />
+          <Box id="3" title="Parent" />
           <Line start="2" end="4" />
           <Line start="3" end="5" />
           <Line start="3" end="6" />
         </div>
         <div className="flex justify-center items-center gap-4">
-          <Box id="4" />
-          <Box id="5" />
-          <Box id="6" />
+          <Box id="4" title="Child" />
+          <Box id="5" title="Child" />
+          <Box id="6" title="Child" />
         </div>
       </div>
     </Xwrapper>
@@ -28,14 +28,14 @@ const ReactContent = () => {
 
 export default ReactContent;
 
-const Box = ({ id }: { id: string }) => {
+const Box = ({ id, title }: { id: string; title?: string }) => {
   return (
     <>
       <div
         id={id}
         className="w-16 h-8 border-2 border-sky-300 rounded-sm flex justify-center items-center"
       >
-        {id}
+        <p className="text-xs">{title}</p>
       </div>
     </>
   );
