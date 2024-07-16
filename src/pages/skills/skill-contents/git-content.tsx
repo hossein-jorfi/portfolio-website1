55b4a1c061ecd2f5d133358cda4acac99a186f19
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-16 18:05:30 +0330

    GitContent

--- src/pages/skills/skill-contents/git-content.tsx
+++ src/pages/skills/skill-contents/git-content.tsx
@@ -4,7 +4,7 @@ import GitBranchIcon from "../../../assets/skills/git-branch.svg";
 const GitContent = () => {
   return (
     <div className="flex justify-center items-center">
-      <img src={GitBranchIcon} className="w-2/3" />
+      <img src={GitBranchIcon} className="h-36" />
     </div>
   );
 };
