438b6046b2d76b76e87b996f54147198a4081c0f
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-16 17:48:01 +0330

    GitContent

--- src/pages/skills/skill-contents/git-content.tsx
+++ src/pages/skills/skill-contents/git-content.tsx
@@ -1,9 +1,11 @@
 const GitContent = () => {
-      return (
-            <div>
-                  GitContent
-            </div>
-      );
+  return (
+    <div className="text-xl">
+      <p>git add .</p>
+      <p>git commit -m "idk"</p>
+      <p>git push</p>
+    </div>
+  );
 };
 
 export default GitContent;
