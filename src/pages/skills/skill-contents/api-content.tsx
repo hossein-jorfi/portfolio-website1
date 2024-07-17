cd708b65a63b80d189434857c81abc9f9b02a217
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-17 12:40:45 +0330

    react query

--- src/pages/skills/skill-contents/api-content.tsx
+++ src/pages/skills/skill-contents/api-content.tsx
@@ -1,20 +1,28 @@
 // assets
 import GraphqlIcon from "@/assets/skills/api/graphql.svg?react";
+import ReactQueryLogo from "@/assets/skills/api/react-query-logo.png";
 
 const LIST = [
   {
     title: "GraphQl",
     icon: <GraphqlIcon width={40} />,
   },
+  {
+    title: "React Query",
+    icon: <img src={ReactQueryLogo} width={40} />,
+  },  
 ];
 
 const ApiContent = () => {
   return (
-    <div>
+    <div className="flex items-baseline gap-3 flex-wrap">
       {LIST.map((item) => (
-        <div key={item.title}>
+        <div
+          key={item.title}
+          className="flex flex-col justify-center items-center"
+        >
           <div>{item.icon}</div>
-          <p></p>
+          <p className="text-sm font-bold">{item.title}</p>
         </div>
       ))}
     </div>
