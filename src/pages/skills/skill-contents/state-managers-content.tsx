8672a475bed21cf2535edafed9a5567637a323fd
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-17 13:22:06 +0330

    StateManagersContent

--- src/pages/skills/skill-contents/state-managers-content.tsx
+++ src/pages/skills/skill-contents/state-managers-content.tsx
@@ -1,5 +1,16 @@
+// import { ReactNode } from "react";
+
+// assets
+
 const StateManagersContent = () => {
   return <div>StateManagersContent</div>;
 };
 
 export default StateManagersContent;
+
+// const ItemContainer = ({ title, icon }: { title: string; icon: ReactNode }) => (
+//   <div className="flex flex-col gap-0.5 justify-center items-center">
+//     <div>{icon}</div>
+//     <p className="text-sm font-bold">{title}</p>
+//   </div>
+// );
