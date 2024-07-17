aab64bdb08abb451bf7ea00ed4935ea0b2db10ee
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-17 12:51:13 +0330

    ApiContent

--- src/pages/skills/skill-contents/api-content.tsx
+++ src/pages/skills/skill-contents/api-content.tsx
@@ -1,32 +1,29 @@
 // assets
 import GraphqlIcon from "@/assets/skills/api/graphql.svg?react";
+import SWRIcon from "@/assets/skills/api/swr.svg?react";
 import ReactQueryLogo from "@/assets/skills/api/react-query-logo.png";
-
-const LIST = [
-  {
-    title: "GraphQl",
-    icon: <GraphqlIcon width={40} />,
-  },
-  {
-    title: "React Query",
-    icon: <img src={ReactQueryLogo} width={40} />,
-  },  
-];
+import { ReactNode } from "react";
 
 const ApiContent = () => {
   return (
-    <div className="flex items-baseline gap-3 flex-wrap">
-      {LIST.map((item) => (
-        <div
-          key={item.title}
-          className="flex flex-col justify-center items-center"
-        >
-          <div>{item.icon}</div>
-          <p className="text-sm font-bold">{item.title}</p>
-        </div>
-      ))}
+    <div className="flex flex-col gap-8">
+      <div className="flex justify-center items-baseline gap-5 flex-wrap">
+        <ItemContainer title="GraphQl" icon={<GraphqlIcon width={40} />} />
+        <ItemContainer
+          title="React Query"
+          icon={<img src={ReactQueryLogo} width={40} />}
+        />
+      </div>
+      <ItemContainer title="SWR" icon={<SWRIcon height={20} />} />
     </div>
   );
 };
 
 export default ApiContent;
+
+const ItemContainer = ({ title, icon }: { title: string; icon: ReactNode }) => (
+  <div className="flex flex-col justify-center items-center">
+    <div>{icon}</div>
+    <p className="text-sm font-bold">{title}</p>
+  </div>
+);
