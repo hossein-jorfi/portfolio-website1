681d0fadd8d9f02494967f5c01c3f67f21eaa0b9
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-17 11:45:37 +0330

    set profit random

--- src/pages/skills/skill-contents/websocket-content.tsx
+++ src/pages/skills/skill-contents/websocket-content.tsx
@@ -1,6 +1,7 @@
 import { LineChart } from "@/components/ui/line-chart";
+import { useEffect, useState } from "react";
 
-const chartData = [
+const CHART_DATA = [
   { month: "January", price: 186, resistance: 80 },
   { month: "February", price: 305, resistance: 200 },
   { month: "March", price: 237, resistance: 120 },
@@ -9,12 +10,50 @@ const chartData = [
   { month: "June", price: 214, resistance: 140 },
 ];
 const WebsocketContent = () => {
+  const [chartData, setChartData] = useState(CHART_DATA);
+  const [profit, setProfit] = useState(10);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setChartData(
+        chartData?.map((item, index) => {
+          if (index === chartData?.length - 1) {
+            return {
+              ...item,
+              price: Math.random() * 1000,
+              resistance: Math.random() * 1000,
+            };
+          } else {
+            return item;
+          }
+        })
+      );
+      setProfit(Math.random() * (100 - -100) + -100);
+    }, 1000);
+    return () => clearInterval(id);
+  }, [chartData, profit]);
+
   return (
     <div>
-      <p>WebsocketContent</p>
+      <Profit value={profit} />
       <LineChart chartData={chartData} />
     </div>
   );
 };
 
 export default WebsocketContent;
+
+const Profit = ({ value }: { value: number }) => {
+  return (
+    <>
+      <p
+        className={`font-bold ${
+          value >= 0 ? "text-green-600" : "text-red-600"
+        }`}
+      >
+        {value >= 0 ? "+" : null}
+        {value}%
+      </p>
+    </>
+  );
+};
