c35aa44968d2b36e97cd0b56e22be881024a79af
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-17 10:56:46 +0330

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        "primary": "#e2e8f0"
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
