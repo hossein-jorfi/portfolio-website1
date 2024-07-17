a8c78c6664f04c206f2487b488d60276f9ca0e3b
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-17 12:32:14 +0330

    ApiContent

--- src/pages/skills/skill-contents/api-content.tsx
+++ src/pages/skills/skill-contents/api-content.tsx
@@ -1,5 +1,24 @@
+// assets
+import GraphqlIcon from "@/assets/skills/api/graphql.svg?react";
+
+const LIST = [
+  {
+    title: "GraphQl",
+    icon: <GraphqlIcon width={40} />,
+  },
+];
+
 const ApiContent = () => {
-  return <div>ApiContent</div>;
+  return (
+    <div>
+      {LIST.map((item) => (
+        <div key={item.title}>
+          <div>{item.icon}</div>
+          <p></p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ApiContent;
