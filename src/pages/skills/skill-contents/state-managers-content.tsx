2b73ee378395c545ab2fc659ef5956cd5172c9f4
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-29 14:06:56 +0330

    StateManagersContent

--- src/pages/skills/skill-contents/state-managers-content.tsx
+++ src/pages/skills/skill-contents/state-managers-content.tsx
@@ -1,16 +1,26 @@
-// import { ReactNode } from "react";
+import { ReactNode } from "react";
 
 // assets
+import ZustandLogo from "../../../assets/skills/state_managers/zustand.svg";
+import ReduxLogo from "../../../assets/skills/state_managers/redux.svg";
 
 const StateManagersContent = () => {
-  return <div>StateManagersContent</div>;
+  return (
+    <div className="flex gap-8 h-full justify-center items-center mt-5">
+      <ItemContainer icon={<img src={ReduxLogo} width={80} />} title="Redux" />
+      <ItemContainer
+        icon={<img src={ZustandLogo} width={80} />}
+        title="Zustand"
+      />
+    </div>
+  );
 };
 
 export default StateManagersContent;
 
-// const ItemContainer = ({ title, icon }: { title: string; icon: ReactNode }) => (
-//   <div className="flex flex-col gap-0.5 justify-center items-center">
-//     <div>{icon}</div>
-//     <p className="text-sm font-bold">{title}</p>
-//   </div>
-// );
+const ItemContainer = ({ title, icon }: { title: string; icon: ReactNode }) => (
+  <div className="flex flex-col gap-0.5 justify-center items-center">
+    <div>{icon}</div>
+    <p className="text-sm font-bold">{title}</p>
+  </div>
+);
