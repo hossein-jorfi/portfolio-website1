a9bd83be3b26fd6e4f538cf29b98a233ee85f8d7
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-07-29 14:20:00 +0330

    UiLibContent

--- src/pages/skills/skill-contents/ui-lib-content.tsx
+++ src/pages/skills/skill-contents/ui-lib-content.tsx
@@ -1,5 +1,26 @@
+import { ReactNode } from "react";
+
+// assets
+import TailwindLogo from '../../../assets/skills/ui_lib/Tailwind.svg'
+
 const UiLibContent = () => {
-  return <div>UiLibContent</div>;
+  return (
+    <div>
+      <div className="flex gap-8 h-full justify-center items-center mt-5">
+        <ItemContainer
+          icon={<img src={TailwindLogo} width={40} />}
+          title="Tailwind"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default UiLibContent;
+
+const ItemContainer = ({ title, icon }: { title: string; icon: ReactNode }) => (
+  <div className="flex flex-col gap-0.5 justify-center items-center">
+    <div>{icon}</div>
+    <p className="text-sm font-bold">{title}</p>
+  </div>
+);
