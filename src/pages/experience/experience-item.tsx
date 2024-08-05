e8893d9389add66b374517097d5c538643760d0c
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-05 09:28:51 +0330

    ExperienceItem

--- src/pages/experience/experience-item.tsx
+++ src/pages/experience/experience-item.tsx
@@ -1,9 +1,10 @@
 const ExperienceItem = () => {
-      return (
-            <div>
-                  ExperienceItem
-            </div>
-      );
+  return (
+    <div className="mt-8 ml-4">
+      <p className="text-5xl font-bold">Maroon Moshaver Arjan</p>
+      <p className="text-4xl font-bold">React Developer</p>
+    </div>
+  );
 };
 
 export default ExperienceItem;
