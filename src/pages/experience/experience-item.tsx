1341bb6d8aea1f5f31487b3b457d11ebe118cbcb
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-05 09:32:21 +0330

    ExperienceItem

--- src/pages/experience/experience-item.tsx
+++ src/pages/experience/experience-item.tsx
@@ -1,10 +1,25 @@
 const ExperienceItem = () => {
   return (
-    <div className="mt-8 ml-4">
-      <p className="text-5xl font-bold">Maroon Moshaver Arjan</p>
-      <p className="text-4xl font-bold">React Developer</p>
+    <div className="mt-8 ml-4 flex gap-4">
+      <TimeLine />
+      <div>
+        <p className="text-5xl font-bold">Maroon Moshaver Arjan</p>
+        <p className="text-4xl font-bold">React Developer</p>
+      </div>
     </div>
   );
 };
 
 export default ExperienceItem;
+
+const TimeLine = () => {
+  return (
+    <div className="flex flex-col">
+      <Circle />
+      <div> line</div>
+      <Circle />
+    </div>
+  );
+};
+
+const Circle = () => <div className="border-2 rounded-full">test</div>;
