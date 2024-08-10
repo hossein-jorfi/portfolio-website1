772a1e314e251995e243a52d31ad7e8aabe2f8a7
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-10 16:32:41 +0330

    assets

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,15 @@
 // components
+import AboutImage from '@/assets/images/about_page_photo.jpg'
 
 const About = () => {
-  return "";
+  return (
+    <div className="text-primary flex justify-between">
+      <div className="border-2 w-1/2">text</div>
+      <div className="w-1/2 border-2">
+      <img src={AboutImage} alt="about" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
