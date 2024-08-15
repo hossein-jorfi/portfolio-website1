de349bb0294a8e5b97d894b233f549777b97570e
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-15 16:44:56 +0330

    MENU_ITEMS

--- src/components/menu-items/mobille-menu.tsx
+++ src/components/menu-items/mobille-menu.tsx
@@ -1,18 +1,23 @@
+import { motion } from "framer-motion";
 import { useState } from "react";
+import { MENU_ITEMS } from "./contants";
+import { Link, useLocation } from "react-router-dom";
 
 const MobilleMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const clickHandler = () => {
     setIsOpen(!isOpen);
-      document.body.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
   };
-  
+
   return (
     <div className="flex sm:hidden justify-end">
       <p onClick={clickHandler}>Menu</p>
       {isOpen && (
         <div className="w-full h-full absolute top-0 right-0 bg-slate-600 z-30">
-          test
+          {MENU_ITEMS.map((item) => (
+            <MenuItem key={item.id} {...item} />
+          ))}
         </div>
       )}
     </div>
@@ -20,3 +25,35 @@ const MobilleMenu = () => {
 };
 
 export default MobilleMenu;
+
+const MenuItem = ({
+  title,
+  to,
+  className,
+}: {
+  title: string;
+  to: string;
+  className?: string;
+}) => {
+  const { pathname } = useLocation();
+  return (
+    <motion.span
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
+      transition={{ duration: 0.5 }}
+      className="relative group inline-block"
+    >
+      <Link className={`${className} `} to={to}>
+        {title}
+        <motion.span
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, ease: "anticipate" }}
+          className={`absolute left-0 -bottom-1 bg-primary h-[2px] group-hover:w-full transition-[width] ease-in-out duration-300 ${
+            to === pathname ? "w-full" : "w-0"
+          }`}
+        ></motion.span>
+      </Link>
+    </motion.span>
+  );
+};
