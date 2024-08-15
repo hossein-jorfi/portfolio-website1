9d69826d7a3fa4d3d7267fb92db5786ca7a3d929
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-15 16:51:13 +0330

    styles

--- src/components/menu-items/mobille-menu.tsx
+++ src/components/menu-items/mobille-menu.tsx
@@ -5,18 +5,22 @@ import { Link, useLocation } from "react-router-dom";
 
 const MobilleMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const clickHandler = () => {
-    setIsOpen(!isOpen);
+  const openHandler = () => {
+    setIsOpen(true);
     document.body.style.overflow = "hidden";
+  };
+  const closeHandler = () => {
+    setIsOpen(false);
+    document.body.style.overflow = "scroll";
   };
 
   return (
     <div className="flex sm:hidden justify-end">
-      <p onClick={clickHandler}>Menu</p>
+      <p onClick={openHandler}>Menu</p>
       {isOpen && (
-        <div className="w-full h-full absolute top-0 right-0 bg-slate-600 z-30">
+        <div className="w-full h-full absolute top-0 right-0 bg-slate-600 flex flex-col items-center">
           {MENU_ITEMS.map((item) => (
-            <MenuItem key={item.id} {...item} />
+            <MenuItem onClose={closeHandler} key={item.id} {...item} />
           ))}
         </div>
       )}
@@ -30,10 +34,12 @@ const MenuItem = ({
   title,
   to,
   className,
+  onClose
 }: {
   title: string;
   to: string;
   className?: string;
+  onClose: () => void
 }) => {
   const { pathname } = useLocation();
   return (
@@ -41,18 +47,10 @@ const MenuItem = ({
       initial={{ opacity: 0, x: -50 }}
       animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
       transition={{ duration: 0.5 }}
-      className="relative group inline-block"
+      className="w-full h-full text-7xl flex justify-center items-center z-[10000]"
     >
-      <Link className={`${className} `} to={to}>
+      <Link onClick={onClose} className={`${className} ${to === pathname && 'bg-slate-950'}`} to={to}>
         {title}
-        <motion.span
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, ease: "anticipate" }}
-          className={`absolute left-0 -bottom-1 bg-primary h-[2px] group-hover:w-full transition-[width] ease-in-out duration-300 ${
-            to === pathname ? "w-full" : "w-0"
-          }`}
-        ></motion.span>
       </Link>
     </motion.span>
   );
