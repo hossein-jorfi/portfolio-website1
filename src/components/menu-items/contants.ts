535fe50e3516c842b48c30fcd429531828aec08a
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-15 16:25:25 +0330

    MENU_ITEMS

---
+++ src/components/menu-items/contants.ts
@@ -0,0 +1,6 @@
+export const MENU_ITEMS = [
+  { id: 0, title: "Home", to: "/" },
+  { id: 1, title: "Skills", to: "/skills" },
+  { id: 3, title: "Experience", to: "/experience" },
+  { id: 2, title: "About", to: "/about" },
+];
