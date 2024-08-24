3b9556d7fe6ff6904cc48a57b5776d98662c90ba
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-24 11:58:57 +0330

    import screen shots

--- src/pages/experience/experience-item/screen-shots/images.tsx
+++ src/pages/experience/experience-item/screen-shots/images.tsx
@@ -1,9 +1,18 @@
+// assets
+import Landing1 from "@/assets/botlyzer/bot1.png";
+import Autotrading from "@/assets/botlyzer/bot_autotrading.png";
+import CreateSrategy from "@/assets/botlyzer/bot_c_srategy.png";
+import Login from "@/assets/botlyzer/bot_login.png";
+
 const Images = () => {
-      return (
-            <div>
-                  Images
-            </div>
-      );
+  return (
+    <div className="flex">
+      <img src={Landing1} alt="screenshot" />
+      <img src={Autotrading} alt="screenshot" />
+      <img src={CreateSrategy} alt="screenshot" />
+      <img src={Login} alt="screenshot" />
+    </div>
+  );
 };
 
 export default Images;
