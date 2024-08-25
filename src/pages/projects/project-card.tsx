8feeb9fefb442cde91d522e1096bfd87e6f137c5
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-25 12:58:27 +0330

    buttons

--- src/pages/projects/project-card.tsx
+++ src/pages/projects/project-card.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 type ProjectCardProps = {
   image: string;
   title: string;
@@ -13,13 +15,22 @@ const ProjectCard = ({
   liveLink,
   title,
 }: ProjectCardProps) => {
-  console.log(gitHubLink, liveLink);
   return (
     <div className="flex gap-4">
       <div className="border-2 p-3 rounded-xl w-[40%] h-auto flex justify-center items-center">
         <img src={image} alt={title} className="rounded-md" />
       </div>
-      <div className="w-[60%]">{text}</div>
+      <div className="w-[60%] flex flex-col justify-between items-start">
+        <div className="">{text}</div>
+        <div className="flex flex-col items-start gap-1">
+          <Button variant="link" className="px-0">
+            <a href={liveLink}>Visit Website</a>
+          </Button>
+          <Button variant="link" className="px-0">
+            <a href={gitHubLink}>See on GitHub</a>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
