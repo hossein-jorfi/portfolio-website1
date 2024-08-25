2b6e8fc4ad6ca9eb6e6a2634970c55af61e79e96
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-25 14:48:32 +0330

    menu items

--- src/components/menu-items/contants.ts
+++ src/components/menu-items/contants.ts
@@ -1,6 +1,7 @@
 export const MENU_ITEMS = [
   { id: 0, title: "Home", to: "/" },
   { id: 1, title: "Skills", to: "/skills" },
-  { id: 3, title: "Experience", to: "/experience" },
-  { id: 2, title: "About", to: "/about" },
+  { id: 2, title: "Experience", to: "/experience" },
+  { id: 3, title: "About", to: "/about" },
+  { id: 4, title: "Projects", to: "/projects" },
 ];
--- src/pages/home/home-buttons.tsx
+++ src/pages/home/home-buttons.tsx
@@ -1,16 +1,24 @@
+import { MENU_ITEMS } from "@/components/menu-items/contants";
+
 // components
 import ButtonType1 from "../../components/button-type1";
 
 const HomeButtons = () => {
+
+  const menuItems = [...MENU_ITEMS]
+  menuItems.shift();
+
   return (
     <div className="flex flex-col sm:flex-row gap-2 md:gap-4">
       <a target="_blank" download={true} href="/Hossein_Jorfi.pdf">
         <ButtonType1>Download CV</ButtonType1>
       </a>
       <DividerLine />
-      <ButtonType1 to="skills">Skills</ButtonType1>
-      <ButtonType1 to="experience">Experience</ButtonType1>
-      <ButtonType1 to="about">About</ButtonType1>
+      {menuItems.map((item) => (
+        <ButtonType1 key={item.id} to={item.to}>
+          {item.title}
+        </ButtonType1>
+      ))}
     </div>
   );
 };
