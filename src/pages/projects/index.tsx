9a1ecdf19ea07188ea44a8171e67275ab82ec6f3
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-25 14:56:27 +0330

    projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -5,24 +5,31 @@ import Landing from "@/assets/projects/landing.png";
 // components
 import ProjectCard from "./project-card";
 
+const PROJECTS = [
+  {
+    id: 0,
+    image: NextDash,
+    title: "Next.js Dashboard",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum culpa nulla at cupiditate, nihil vel explicabo, nostrum voluptatibus nesciunt excepturi possimus mollitia veritatis delectus reiciendis harum libero beatae porro ad!",
+    liveLink: "https://nextjs-dashboard-hossein.vercel.app",
+    gitHubLink: "https://github.com/hossein-jorfi/nextjs-dashboard",
+  },
+  {
+    id: 1,
+    image: Landing,
+    title: "Landing page",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum culpa nulla at cupiditate, nihil vel explicabo, nostrum voluptatibus nesciunt excepturi possimus mollitia veritatis delectus reiciendis harum libero beatae porro ad!",
+    liveLink: "https://landing-page-neon-five.vercel.app",
+    gitHubLink: "https://github.com/hossein-jorfi/landing-page",
+  },
+];
+
 const Projects = () => {
   return (
     <div className="text-primary px-3 space-y-10 mt-5">
-      <ProjectCard
-        image={NextDash}
-        title="Next.js Dashboard"
-        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum culpa nulla at cupiditate, nihil vel explicabo, nostrum voluptatibus nesciunt excepturi possimus mollitia veritatis delectus reiciendis harum libero beatae porro ad!"
-        liveLink=""
-        gitHubLink=""
-      />
-      {/* <div className="w-full h-[1px] bg-primary/40" /> */}
-      <ProjectCard
-        image={Landing}
-        title="Next.js Dashboard"
-        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum culpa nulla at cupiditate, nihil vel explicabo, nostrum voluptatibus nesciunt excepturi possimus mollitia veritatis delectus reiciendis harum libero beatae porro ad!"
-        liveLink=""
-        gitHubLink=""
-      />
+      {PROJECTS.map((project) => (
+        <ProjectCard {...project} key={project.id} />
+      ))}
     </div>
   );
 };
