ca5ef355470da524b98bd4ba9a260a1909e5cc0f
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-25 12:50:03 +0330

    landing project assets

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,6 @@
 // assets
 import NextDash from "@/assets/projects/next-dash.png";
+import Landing from "@/assets/projects/landing.png";
 
 // components
 import ProjectCard from "./project-card";
@@ -9,6 +10,13 @@ const Projects = () => {
     <div className="text-primary px-3 space-y-10 mt-5">
       <ProjectCard
         image={NextDash}
+        title="Next.js Dashboard"
+        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum culpa nulla at cupiditate, nihil vel explicabo, nostrum voluptatibus nesciunt excepturi possimus mollitia veritatis delectus reiciendis harum libero beatae porro ad!"
+        liveLink=""
+        gitHubLink=""
+      />
+      <ProjectCard
+        image={Landing}
         title="Next.js Dashboard"
         text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum culpa nulla at cupiditate, nihil vel explicabo, nostrum voluptatibus nesciunt excepturi possimus mollitia veritatis delectus reiciendis harum libero beatae porro ad!"
         liveLink=""
--- src/pages/projects/project-card.tsx
+++ src/pages/projects/project-card.tsx
@@ -16,7 +16,7 @@ const ProjectCard = ({
   console.log(gitHubLink, liveLink);
   return (
     <div className="flex gap-4">
-      <div className="border-2 p-1 rounded-xl w-[40%] h-auto flex justify-center items-center">
+      <div className="border-2 p-3 rounded-xl w-[40%] h-auto flex justify-center items-center">
         <img src={image} alt={title} className="rounded-md" />
       </div>
       <div className="w-[60%]">{text}</div>
