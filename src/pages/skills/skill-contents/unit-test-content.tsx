2faa52db44f457ca9221799c35f4d0bd51ea9fd1
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 17:41:36 +0330

    UnitTestContent

--- src/pages/skills/skill-contents/unit-test-content.tsx
+++ src/pages/skills/skill-contents/unit-test-content.tsx
@@ -8,13 +8,19 @@ const UnitTestContent = () => {
     <div className="flex gap-2">
       <div className="flex flex-col gap-2 w-full">
         <UnitTestItem count={1} type="pass" />
-        <UnitTestItem count={1} type="pass" className="ml-7" />
+        <UnitTestItem count={1} type="pass" className="ml-5" />
         <UnitTestItem count={1} type="failed" />
-        <UnitTestItem count={1} type="failed" className="ml-7" />
+        <UnitTestItem count={1} type="failed" className="ml-5" />
       </div>
-      <div className="flex flex-col items-center w-full">
-        <VitestIcon className="w-6 h-6" />
-        <JestIcon className="w-6 h-6" />
+      <div className="flex flex-col justify-between items-center w-full gap-2">
+        <div className="flex flex-col  items-center">
+          <VitestIcon className="w-10 h-10" />
+          <p className="text-sm font-semibold">Vitest</p>
+        </div>
+        <div className="flex flex-col  items-center">
+          <JestIcon className="w-10 h-10" />
+          <p className="text-sm font-semibold">Jest</p>
+        </div>
       </div>
     </div>
   );
@@ -49,7 +55,7 @@ const UnitTestItem = ({ type, count, className }: UnitTestItemProps) => {
           <X className="w-[15px] h-[15px]" />
         )}
       </div>
-      <p>
+      <p className="text-nowrap">
         {count} {type}
       </p>
     </div>
