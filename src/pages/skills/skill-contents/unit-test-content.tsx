018ab20dcb652d8a9b7a7adbf22be3414889878a
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 17:24:09 +0330

    assets

--- src/pages/skills/skill-contents/unit-test-content.tsx
+++ src/pages/skills/skill-contents/unit-test-content.tsx
@@ -1,13 +1,21 @@
 import { cn } from "@/lib/utils";
 import { Check, X } from "lucide-react";
+import VitestIcon from "@/assets/skills/vitest.svg?react";
+import JestIcon from "@/assets/skills/jest.svg?react";
 
 const UnitTestContent = () => {
   return (
-    <div className="flex flex-col gap-2">
-      <UnitTestItem count={1} type="pass" />
-      <UnitTestItem count={1} type="pass" className="ml-7" />
-      <UnitTestItem count={1} type="failed" />
-      <UnitTestItem count={1} type="failed" className="ml-7" />
+    <div className="flex gap-2">
+      <div className="flex flex-col gap-2 w-full">
+        <UnitTestItem count={1} type="pass" />
+        <UnitTestItem count={1} type="pass" className="ml-7" />
+        <UnitTestItem count={1} type="failed" />
+        <UnitTestItem count={1} type="failed" className="ml-7" />
+      </div>
+      <div className="flex flex-col items-center w-full">
+        <VitestIcon className="w-6 h-6" />
+        <JestIcon className="w-6 h-6" />
+      </div>
     </div>
   );
 };
@@ -24,7 +32,7 @@ const UnitTestItem = ({ type, count, className }: UnitTestItemProps) => {
   return (
     <div
       className={cn(
-        "flex gap-3",
+        "flex gap-3 font-semibold",
         type === "pass" ? "text-green-500" : "text-red-500",
         className
       )}
