6f1559596f69e043bf56e29e01c8801a6336f378
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 17:07:31 +0330

    styles

--- src/pages/skills/skill-contents/unit-test-content.tsx
+++ src/pages/skills/skill-contents/unit-test-content.tsx
@@ -1,11 +1,12 @@
-import { Check, Cross2Icon } from "lucide-react";
+import { Check, X } from "lucide-react";
 
 const UnitTestContent = () => {
   return (
-    <div>
-      <UnitTestItem count={2} type="pass" />
-      <UnitTestItem count={2} type="pass" />
-      <UnitTestItem count={2} type="pass" />
+    <div className="flex flex-col gap-2">
+      <UnitTestItem count={1} type="pass" />
+      <UnitTestItem count={1} type="pass" />
+      <UnitTestItem count={1} type="failed" />
+      <UnitTestItem count={1} type="failed" />
     </div>
   );
 };
@@ -19,8 +20,18 @@ type UnitTestItemProps = {
 
 const UnitTestItem = ({ type, count }: UnitTestItemProps) => {
   return (
-    <div className="flex gap-3">
-      icon
+    <div
+      className={`flex gap-3 ${
+        type === "pass" ? "text-green-500" : "text-red-500"
+      }`}
+    >
+      <div className="border-2 rounded-full p-1">
+        {type === "pass" ? (
+          <Check className="w-[15px] h-[15px]" />
+        ) : (
+          <X className="w-[15px] h-[15px]" />
+        )}
+      </div>
       <p>
         {count} {type}
       </p>
