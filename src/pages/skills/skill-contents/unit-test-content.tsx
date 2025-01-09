632dcd69fd27c521ab89cd64edaa32f923461b66
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 16:07:23 +0330

    UnitTestItem

--- src/pages/skills/skill-contents/unit-test-content.tsx
+++ src/pages/skills/skill-contents/unit-test-content.tsx
@@ -1,9 +1,29 @@
+import { Check, Cross2Icon } from "lucide-react";
+
 const UnitTestContent = () => {
   return (
     <div>
-      <p>UnitTestContent</p>
+      <UnitTestItem count={2} type="pass" />
+      <UnitTestItem count={2} type="pass" />
+      <UnitTestItem count={2} type="pass" />
     </div>
   );
 };
 
 export default UnitTestContent;
+
+type UnitTestItemProps = {
+  type: "pass" | "failed";
+  count: number;
+};
+
+const UnitTestItem = ({ type, count }: UnitTestItemProps) => {
+  return (
+    <div className="flex gap-3">
+      icon
+      <p>
+        {count} {type}
+      </p>
+    </div>
+  );
+};
