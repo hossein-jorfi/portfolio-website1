bf075c29662af17376eec8cfa8f0991da94ecd1f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-05 10:47:57 +0330

    update ScreenShots

--- src/pages/experience/experience-item/screen-shots/index.tsx
+++ src/pages/experience/experience-item/screen-shots/index.tsx
@@ -1,14 +1,18 @@
+import { Button } from "@/components/ui/button";
 import Images from "./images";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { Link } from "lucide-react";
 
 const ScreenShots = () => {
   return (
     <div className="sm:px-7">
-      <p className="text-xl">
-        The website has not yet been published. however, I have included some
-        screenshots for your reference.
-      </p>
-      <div className="flex items-center gap-1 text-slate-400 mt-0">
+      <Button variant="link" className="text-2xl -ml-4" asChild>
+        <a href="https://botlyzer.ir" target="_blank">
+          <Link className="mr-2 size-5" />
+          botlyzer.ir
+        </a>
+      </Button>
+      <div className="flex items-center gap-1 text-slate-400 mt-2">
         <InfoCircledIcon />
         <p className="text-sm">Click the photo to view it on a bigger size.</p>
       </div>
